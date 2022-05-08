e9df97ccbba049762fdd03bf9e59c7e2907fbafa
yohansmallaxe <yohan@smallaxe.tech>
2022-05-08 21:43:40 +0530

    removed  weather Middleware from global route middleware

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
 Route::get('/', function () {
+    return redirect()->route('dashboard');
+});
+
+Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware('weather')->name('dashboard');
-///->middleware(['auth'])->name('dashboard')
+})->middleware(['auth'])->name('dashboard');
 require __DIR__.'/auth.php';
